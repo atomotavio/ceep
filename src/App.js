22142ef2d7dfacf9f9ea3b33b9b8f07ee0d455ac
Mariana de Carvalho Trindade <contatomarilop@gmail.com>
2021-09-21 14:34:36 -0300

    alteraçoes na organizacao do projeto, adicao de estilo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Component } from 'react';
-import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
-import ListaDeNotas from './components/ListaDeNotas/ListaDeNotas';
-
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
   render() {
     return (
-      <>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
-      </>
+      </section>
     );
   }
-  
 }
 
 export default App;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,25 @@
-import { Component } from "react";
-
+import React, { Component } from "react";
+import "./estilo.css";
 class FormularioCadastro extends Component {
-    render() {
-        return (
-            <form>
-                <input type="text" placeholder="Título" />
-                <textarea placeholder="Escreva sua nota..."
-                />
-                <button>Criar nota</button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form className="form-cadastro ">
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
+  }
 }
 
 export default FormularioCadastro;
